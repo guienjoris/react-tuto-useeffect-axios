fe7ce4b0f74bf0120e28c11cef07092cc44206e4
Shinryu <guienjoris@gmail.com>
2020-04-30 11:42:31 +0200

    tuto react useEffect and axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Fragment, useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [data,setData] = useState([]);
+  
+  // Similaire à "componentDidMount" et "componentDidUpdate";
+
+  useEffect(()=>{
+    const fetchData = async ()=>{
+      const result = await axios(
+        'https://jsonplaceholder.typicode.com/posts',
+      )
+      setData(result.data)
+      console.log('updated');
+    };
+    fetchData();
+    console.log('mounted');
+  },[]) // [] important sinon boucle infini sur la requete
+   console.log(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <ul>
+        {data.map(item=>(
+          <li key= {item.id}>
+            <p>{item.title}</p>
+          </li>
+        ))}
+      </ul>
+    </Fragment>
   );
 }
 
